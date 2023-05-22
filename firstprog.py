557ff5906c62aa6b931f29d43f3335b945d0081f
ulfat612001 <88612569+ulfat612001@users.noreply.github.com>
2023-05-22 15:14:43 +0530

    Update firstprog.py

--- firstprog.py
+++ firstprog.py
@@ -9,5 +9,5 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index
 fruits_to_show = my_fruit_list.loc[fruits_selected]                                               
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
