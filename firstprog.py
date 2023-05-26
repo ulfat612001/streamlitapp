c350a6a57a1f907bac4750c86a4ae4a23484050c
ulfat612001 <88612569+ulfat612001@users.noreply.github.com>
2023-05-26 15:06:06 +0530

    Update firstprog.py

--- firstprog.py
+++ firstprog.py
@@ -11,3 +11,6 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]                                               
 streamlit.dataframe(fruits_to_show)
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
