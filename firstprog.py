7f37ceb7ecbbcd9d81ac67365bd5574c5a6f7984
ulfat612001 <88612569+ulfat612001@users.noreply.github.com>
2023-05-29 14:21:07 +0530

    Update firstprog.py

--- firstprog.py
+++ firstprog.py
@@ -34,4 +34,5 @@ streamlit.dataframe(my_data_row)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add','Jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
