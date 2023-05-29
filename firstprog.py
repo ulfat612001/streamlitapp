572219217b2f73b3b2e0ed49150a333c4e2628be
ulfat612001 <88612569+ulfat612001@users.noreply.github.com>
2023-05-29 12:58:12 +0530

    Update firstprog.py

--- firstprog.py
+++ firstprog.py
@@ -27,8 +27,8 @@ streamlit.write('The user entered ', fruit_choice)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
-select * from pc_rivery_db.public.fruit_load_list
+
