50606268142cb5c89283e9795650f8dbec43c79e
ulfat612001 <88612569+ulfat612001@users.noreply.github.com>
2023-05-29 12:56:48 +0530

    Update firstprog.py

--- firstprog.py
+++ firstprog.py
@@ -31,3 +31,4 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+select * from pc_rivery_db.public.fruit_load_list
