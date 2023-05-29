bbe71eb0512f6f762efd06721e888b013c6fd4fe
ulfat612001 <88612569+ulfat612001@users.noreply.github.com>
2023-05-29 14:31:56 +0530

    Update firstprog.py

--- firstprog.py
+++ firstprog.py
@@ -1,18 +1,21 @@
 import streamlit
+import pandas
 import snowflake.connector
+import requests
+from urllib.error import URLError
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]                                               
 streamlit.dataframe(fruits_to_show)
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.header("Fruityvice Fruit Advice!")
